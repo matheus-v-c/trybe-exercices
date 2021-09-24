13870e5c40d966db7f3e4f63ce4a74ae613ceb8a
Matheus Vieira <matheus.vieira@betrybe.com>
2021-09-24 13:59:52 -0300

    Item 5 do exercício

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/exercicio-1/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/exercicio-1/index.js
@@ -13,4 +13,20 @@ if (peso <= 0 || altura <= 0) {
 }
 
 const imc = imcFunc.imcCalc(peso, altura);
+
+function ImcCategory(imc, situacao, estado) {
+    this.imc = imc;
+    this.situacao = situacao;
+    this.estado = estado;
+}
+
 console.log(`O seu IMC é de ${imc}!`);
+
+var magreza = new ImcCategory('Abaixo de 18,5', 'Abaixo do peso (magreza)', imc < 18.5);
+var normal = new ImcCategory('Entre 18,5 e 24,9', 'Peso normal', imc >= 18.5 && imc < 25);
+var sobrepeso = new ImcCategory('Entre 25,0 e 29,9', 'Acima do peso (sobrepeso)', imc >= 25 && imc < 30);
+var obesidadeGrau1 = new ImcCategory('Entre 30,0 e 34,9', 'Obesidade grau I', imc >= 30 && imc < 35);
+var obesidadeGrau2 = new ImcCategory('Entre 35,0 e 39,9', 'Obesidade grau II', imc >= 35 && imc < 40);
+var obesidadeGrau3 = new ImcCategory('40,0 e acima', 'Obesidade graus III e IV', imc >= 40);
+
+console.table([magreza, normal, sobrepeso, obesidadeGrau1, obesidadeGrau2, obesidadeGrau3]);
